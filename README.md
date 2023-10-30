0f3e7bb0f2671db6f192d52991d0e453dd3227a8
Ryan Peay <ryanp@skeeg.net>
2023-10-30 15:23:55 -0600

    Some basic doc updates

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # HomeAssistant
+Using git bare and sparse configs requires a later version of git than is natively on raspberry pi.  Please read/execute `build_git_2.42.0.sh` before diving all the way in on this.
---
+++ build_git_2.42.0.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Download and build 2.42.0 git on Raspberry Pi
+apt update
+apt install -y make libssl-dev libghc-zlib-dev libcurl4-gnutls-dev libexpat1-dev gettext autoconf
+cd /tmp/ || exit
+wget https://github.com/git/git/archive/refs/tags/v2.42.0.tar.gz
+tar -xzvf v2.42.0.tar.gz
+cd /tmp/git-2.42.0/ || exit
+./configure --prefix=/usr/local
+make all
+sudo make install
+git --version
+
