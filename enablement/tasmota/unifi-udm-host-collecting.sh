4e9a951cb7eb9a8a82f2d0703ab00566c836741f
Ryan Peay <ryan-peay@pluralsight.com>
2025-11-04 19:14:22 -0700

    Start of refreshing state of project, dump and move

--- enablement/tasmota/unifi-udm-host-collecting.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-udm-login 10.2.2.1 "$(secret get UDM_USERNAME)" "$(secret get UDM_PASSWORD)"
-
-echo "[" > at.raw
-active-tasmotas | jq -c ". | [.mac, .ip]" | sed 's/,/,\"ip\":/' | sed 's/\[/{\"mac":/' | sed 's/\]/},/' >> at.raw
-echo "]" >> at.raw
-tr '\n' ' ' < at.raw | sed 's/,[[:space:]]\]/\]/g' | jq -c '.' > at.json
--- firmware/README.md
+++
@@ -1,3 +0,0 @@
-# Tasmota firmware files go in this directory
-
-Might need to update the information in the service configuration if you are not running as the `pi` user with files generally under `/home/pi/docker`
